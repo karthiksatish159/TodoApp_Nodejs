4d6c609254fb1d564328acc46af030064433a09a
karthiksatish159 <74001872+karthiksatish159@users.noreply.github.com>
2022-06-11 11:43:59 +0530

    second2

--- index.js
+++ index.js
@@ -8,15 +8,12 @@ const app=express();
 const nodemailer = require('nodemailer');
 const auth=require('./routes/public/auth');
 const todo=require('./routes/public/crud');
-const port=process.env.PORT||4000;
+const port=process.env.PORT||3000;
 const ejs=require('ejs');
 app.use(bodyparser.urlencoded({extended:false}));
 app.use(bodyparser.json());
 const cookieParser = require('cookie-parser');
 app.use(cookieParser());
-app.use(cors({
-    origin:"http://localhost:3000",
-}))
 //So here we are calling the views directory
 app.set('views',__dirname+"/"+"views");
 app.set('view engine','ejs');
@@ -26,10 +23,14 @@ mongoose.connect(db,{useNewUrlParser: true})
 .catch(err=>console.log(err));
 app.use(passport.initialize());
 require("./strategies/jwtstrategy")(passport);
-app.get("/",(req,res)=>
+app.use("/",(req,res)=>
 {
     res.send("Hello word");
 })
+// app.get("/",(req,res)=>
+// {
+//     res.send("Hello word");
+// })
 //routes
 app.use('/auth',auth);
 app.use('/todo',todo);
